e94e550e006e680a2c376d2e8fa59d9d880c6748
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-27 18:20:04 -0300

    First commit

---
+++ src/main/java/com/esdrasdev/design_pattern_strategy/DesignPatternStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.design_pattern_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternStrategyApplication.class, args);
+	}
+
+}
