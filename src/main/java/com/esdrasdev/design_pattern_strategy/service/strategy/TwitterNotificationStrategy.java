43183928e03a53ba4999ac1f8ea5dd9ffefb6b7e
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-12-08 12:12:29 -0300

    create class twitter

--- src/main/java/com/esdrasdev/design_pattern_strategy/service/strategy/TwitterNotificationStrategy.java
+++ src/main/java/com/esdrasdev/design_pattern_strategy/service/strategy/TwitterNotificationStrategy.java
@@ -1,10 +1,15 @@
 package com.esdrasdev.design_pattern_strategy.service.strategy;
 
 import com.esdrasdev.design_pattern_strategy.service.NotificationStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TwitterNotificationStrategy implements NotificationStrategy {
+
+    private Logger logger = LoggerFactory.getLogger(TwitterNotificationStrategy.class);
+
     @Override
     public void sendNotification(String destination, String message) {
-
+        logger.info("Notificacao [{}] enviada para o Twitter [{}]", message, destination);
     }
 }
