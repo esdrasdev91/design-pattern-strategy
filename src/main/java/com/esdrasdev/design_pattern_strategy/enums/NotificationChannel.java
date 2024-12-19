ec61cbb538d7f568fd269025161787828038f229
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-12-19 11:55:36 -0300

    add new class

--- src/main/java/com/esdrasdev/design_pattern_strategy/enums/NotificationChannel.java
+++ src/main/java/com/esdrasdev/design_pattern_strategy/enums/NotificationChannel.java
@@ -2,14 +2,15 @@ package com.esdrasdev.design_pattern_strategy.enums;
 
 import com.esdrasdev.design_pattern_strategy.service.NotificationStrategy;
 import com.esdrasdev.design_pattern_strategy.service.strategy.DiscordNotificationStrategy;
+import com.esdrasdev.design_pattern_strategy.service.strategy.EmailNotificationStrategy;
 import com.esdrasdev.design_pattern_strategy.service.strategy.InstagramNotificationStrategy;
 import com.esdrasdev.design_pattern_strategy.service.strategy.TwitterNotificationStrategy;
 
 public enum NotificationChannel {
     DISCORD(new DiscordNotificationStrategy()),
     INSTAGRAM(new InstagramNotificationStrategy()),
-    TWITTER(new TwitterNotificationStrategy());
-    EMAIL(new EmailNotificationStrategy()),
+    TWITTER(new TwitterNotificationStrategy()),
+    EMAIL(new EmailNotificationStrategy());
 //    WHATSAPP(new WhatsappNotificationStrategy());
 
     private final NotificationStrategy strategy;
